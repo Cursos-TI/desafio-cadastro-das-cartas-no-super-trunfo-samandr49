649114458758bd3b478b3b6041f31ff4e54c4e56
samandr49 <s23195743@gmail.com>
2025-05-29 19:12:18 +0000

    .

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,50 +2,14 @@
     #include <stdio.h>
 
    int main(){  
-    printf(char [81])"Digite a letra que deseja utilizar para identificar o primeiro estado (A - H): \n"
-    scanf("%c", &letra1);
-  
-    printf("Digite o nome deste estado: \n");
-    scanf(" %49s", nomeest1);
-  
-    printf("Digite o número da primeira cidade, de %s (1 a 4): \n", nomeest1);
-    scanf(" %d", &numero11);
-
-    printf("Digite o nome da primeira cidade, de %s: \n", nomeest1);
-    scanf(" %49s", nomecid11);
-
-    printf("Digite a população de %s: \n", nomecid11);
-    scanf(" %d", &populacao11);
-
-    printf("Digite a área de %s em Km2: \n", nomecid11);
-    scanf(" %f", &area11);
-      
-    printf("Digite o PIB de %s em reais: \n", nomecid11);
-    scanf(" %f", &pib11);
-    
-    printf("Digite o número de pontos turísticos em %s: \n", nomecid11);
-    scanf(" %d", &turistico11);
-
-    printf("Digite o número da segunda cidade, de %s (1 a 4): \n", nomeest1); 
-    scanf(" %d", &numero12);
-
-    printf("Digite o nome da segunda cidade, de %s: \n", nomeest1);
-    scanf(" %49s", nomecid12);
+ char Estado ;
+ char código da carta ;
+ char nome da cidade ;
+ int pouplação ;
+ float Àrea em km2 ;
+ float PIB ;
+ int   Numero de Pontos Turísticos ;
  
-    printf("Digite a população de %s: \n", nomecid12);
-    scanf(" %d", &populacao12);
-
-   printf("Digite a população de %s: \n", nomecid12);
-    scanf(" %d", &populacao12);
-
-   printf("Digite a área de %s em Km2: \n", nomecid12);
-   scanf(" %f", &area12);
-
-   printf("Digite o PIB de %s em reais: \n", nomecid12);
-   scanf(" %f", &pib12);
-
-   printf("Digite o número de pontos turísticos em %s: \n", nomecid12);
-   scanf(" %d\n", &turistico12);
 
 
 
@@ -54,4 +18,49 @@
 
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+    
